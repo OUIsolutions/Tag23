229289b20f69b12c7d89204e7f095e4a200b8d3b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-15 22:17:53 -0300

    att

---
+++ build.py
@@ -0,0 +1,88 @@
+from os import listdir
+import hashlib
+from shutil import rmtree
+from os import makedirs
+
+SOURCES = [
+    "src/constants.js",
+    "src/extra.js",
+    "src/actions/conditions.js",
+    "src/actions/for.js",
+    "src/actions/geral.js",
+
+    "src/main.js"
+]
+
+LIB_NAME = 'Tag23'
+REPO_NAME = 'OUIsolutions/Tag23'
+
+
+def create_output():
+    output = ''
+    for e in SOURCES:
+        with open(e, 'r') as f:
+            output += f.read() + '\n'
+    version = input('version: ')
+
+    makedirs('versions', exist_ok=True)
+
+    output_name = f'versions/{LIB_NAME}_v{version}.js'
+
+    with open(output_name, 'w') as f:
+        f.write(output)
+    return output_name
+
+
+output_name = create_output()
+#replacing html links 
+link = f'https://cdn.jsdelivr.net/gh/{REPO_NAME}@main/{output_name}'
+div = f'src="{link}"'
+
+
+rmtree('internal/exemples',ignore_errors=True)
+makedirs('internal/exemples')
+
+for e in listdir('internal/exemples_not_linked'):
+    with open(f'internal/exemples_not_linked/{e}', 'r') as f:
+        output = f.read()
+        output = output.replace('#lib#', div)
+        with open(f'internal/exemples/{e}', 'w') as f:
+            f.write(output)
+
+
+exemples = listdir('internal/exemples')
+
+with open('internal/readme.md', 'r') as f:
+    readme_code = f.read()
+
+
+for e in exemples:
+    with open(f'internal/exemples/{e}', 'r') as f:
+        output = f.read()
+        output_data =''
+        output_data+= '[Runable exemple]'
+        output_data+=f'(https://ouisolutions.github.io/{REPO_NAME}/internal/exemples/{e})'
+        output_data+=f'\n```html\n{output}\n```'
+        readme_code = readme_code.replace(f"#page_ref:{e}", output_data)
+
+
+for e in exemples:
+    with open(f'internal/exemples/{e}', 'r') as f:
+        output = f.read()
+        output_data=f'\n```html\n{output}\n```'
+        readme_code = readme_code.replace(f"#ref:{e}", output_data)
+
+
+if "#page_ref" in readme_code:
+    index = readme_code.find("#page_ref")
+    reference = readme_code[index: readme_code.find("\n", index)]
+    raise Exception(f"Missing reference {reference}")
+
+if "#ref" in readme_code:
+    
+    index = readme_code.find("#ref")
+    reference = readme_code[index: readme_code.find("\n", index)]
+    raise Exception(f"Missing reference {reference}")
+
+with open('readme.md', 'w') as f:
+    f.write(readme_code)
---
+++ versions/Tag23_v0.1.js
@@ -0,0 +1,391 @@
+const TAG_23_START = "start";
+const TAG_23_CASE = "case";
+const TAG_23_UNLESS = "unless"
+const TAG_23_SET_VALUE = "set_value";
+const TAG_23_DEFAULT_VALUE = "default_value";
+const TAG_23_PRINT = "print";
+const TAG_23_EVALUATED = "evaluated"
+const TAG_23_FOR = "for";
+const  TAG_23_IN = "in";
+
+const TAG_23_INDEX = "index";
+const TAG_23_DESTROY = "destroy";
+const TAG_23_HIDE ="none";
+const  TAG_23_SHOW = 'block';
+
+const TAG_23_TICK_DEFAULT_TIME = 100;
+
+/**
+ * @param {HTMLElement} element
+ * @param {function || string } text
+ * @return {string  || any}
+ * */
+function tag23get_evaluation_result(element,text){
+
+    let callback = function(){return eval(text)};
+    while (callback instanceof Function) {
+
+        callback = callback.call(element);
+        
+    }
+    return callback;
+
+
+}
+/**
+ * @param {HTMLElement} target
+ * @param {string} name
+ * @param {any} value
+ * */
+function set_value_recursively_in_element(target,name,value){
+    for(let i=0;i<target.children.length;i++) {
+        let child = target.children[i];
+        child[name] = value;
+        set_value_recursively_in_element(child,name,value);
+    }
+
+}
+/**@param {Tag23LoopProps} loop_props
+ * */
+function tag23_case(loop_props){
+
+    let current_element = loop_props.current_element;
+
+    let condition = current_element.getAttribute(TAG_23_CASE);
+    let result = tag23get_evaluation_result(current_element,condition);
+
+    if(result){
+        current_element.style.display = TAG_23_SHOW;
+    }
+    if(!result){
+        current_element.style.display = TAG_23_HIDE;
+        loop_props.skip = true;
+    }
+}
+
+/**@param {Tag23LoopProps} loop_props
+ * */
+function tag23_unless(loop_props){
+    let current_element = loop_props.current_element;
+
+    let condition = current_element.getAttribute(TAG_23_UNLESS);
+    let result = tag23get_evaluation_result(current_element,condition);
+
+    if(result){
+        current_element.style.display = TAG_23_HIDE;
+        loop_props.skip = true;
+    }
+
+    if(!result){
+        current_element.style.display = TAG_23_SHOW;
+    }
+
+
+
+}
+
+
+function tag23_get_old_elements(father,index,iterator){
+    let elements = [];
+    for(let i = 0;i< father.children.length;i++){
+        let point = i+index+1;
+        let current = father.children[point];
+        if(!current){
+            break;
+        }
+
+        if(!current.hasAttribute(TAG_23_INDEX)){
+            let name = current.getAttribute(TAG_23_IN);
+            if(name !== iterator){
+                break;
+            }
+        }
+
+
+        elements.push({
+            index: parseInt(current.getAttribute(TAG_23_INDEX)),
+            order:i,
+            element:current
+        });
+    }
+    return elements;
+}
+
+
+
+/**@param {Tag23LoopProps} loop_props
+ */
+function tag23_for(loop_props){
+    loop_props.skip = true;
+    let current = loop_props.current_element;
+    current.style.display = TAG_23_HIDE;
+
+    let value_as = current.getAttribute(TAG_23_FOR);
+    if(!value_as){
+        console.log("for value not provided")
+        return;
+    }
+    let index_name = `${value_as}_${TAG_23_INDEX}`
+    let destroy_name = `${value_as}_${TAG_23_DESTROY}`
+    let array_name =   current.getAttribute(TAG_23_IN);
+    if(!array_name){
+        console.log("in not provided")
+        return;
+    }
+    /**@type {Array<any>}*/
+    let array_value = tag23get_evaluation_result(current,array_name);
+    if(!array_value){
+        return;
+    }
+    //iterated over the brothers of child
+    let father = current.parentNode;
+    let elements = tag23_get_old_elements(father,loop_props.index,array_name);
+
+
+
+    for(let j=0;j<array_value.length;j++){
+        //verify if j its not present on elements
+        let found = false;
+        for(let k=0;k<elements.length;k++){
+            if(elements[k].index === j){
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            continue;
+        }
+
+        let created = current.cloneNode(true);
+        created.style.display = TAG_23_SHOW;
+        //remove attribute for
+        created.removeAttribute(TAG_23_FOR);
+
+        set_value_recursively_in_element(created,value_as,()=>{
+            return array_value[j];
+        })
+
+        set_value_recursively_in_element(created,index_name,()=>{
+            return j;
+        })
+
+        set_value_recursively_in_element(created,destroy_name,()=>{
+            array_value.splice(j,1);
+        })
+
+
+        created.setAttribute(TAG_23_INDEX,j);
+        elements.push({
+            index:j,
+            element:created
+        });
+    }
+
+
+
+    //sort elements by index
+    elements.sort((a,b)=>a.index-b.index);
+    let its_all_correct = true;
+    for(let j=0;j<elements.length;j++){
+        if(elements[j].order !== j){
+            its_all_correct = false;
+            break;
+        }
+    }
+
+    if(its_all_correct && elements.length === array_value.length){
+        return;
+    }
+
+    //console.log(elements);
+    //console.log(array_value);
+
+
+    let fragment = document.createDocumentFragment();
+    for(let j=0;j<elements.length;j++){
+        //verify if j its not present on elements
+        let current = elements[j];
+
+
+
+        if(current.element.parentNode === father){
+            current.element = father.removeChild(current.element);
+        }
+        if(current.index <= array_value.length -1){
+            fragment.appendChild(current.element);
+        }
+
+    }
+    father.insertBefore(fragment,current.nextSibling);
+
+
+
+}
+
+
+
+
+
+
+/**@param {HTMLElement} current_element
+ */
+function tag23_start(current_element){
+    current_element.style.display = TAG_23_SHOW;
+}
+
+
+/**@param {HTMLElement || HTMLInputElement} current_element
+ */
+function tag23_value(current_element){
+
+    if(current_element.getAttribute(TAG_23_SET_VALUE)){
+        return;
+    }
+    if(current_element.getAttribute(TAG_23_EVALUATED)){
+        return;
+    }
+
+    let tag_data = current_element.getAttribute(TAG_23_DEFAULT_VALUE);
+    current_element.value = tag23get_evaluation_result(current_element,tag_data);
+    current_element.setAttribute(TAG_23_EVALUATED,'true');
+
+}
+
+/**@param {HTMLInputElement} current_element
+ */
+function tag23_set_value(current_element){
+
+    let name_of_var = current_element.getAttribute(TAG_23_SET_VALUE);
+
+    if(document.activeElement === current_element){
+        let var_type = undefined;
+        try{
+            //the variable cannot exist
+            eval(`var_type =${name_of_var}.constructor.name`);
+        }catch (error){}
+        if(var_type === 'Number'){
+            eval(`${name_of_var} = Number(current_element.value)`);
+        }
+
+        if(var_type !== 'Number'){
+            eval(`${name_of_var} = current_element.value`);
+        }
+
+    }
+
+    if(document.activeElement !== current_element){
+
+        try {
+            //the variable cannot exist
+            let value = tag23get_evaluation_result(current_element,name_of_var);
+            if(value !== undefined && value !== null){
+                current_element.value = value;
+            }
+        }catch (error){}
+
+    }
+
+
+}
+
+
+/**@param {HTMLElement} current_element
+ */
+function tag23_print(current_element){
+    let text = current_element.getAttribute(TAG_23_PRINT);
+    current_element.innerHTML = tag23get_evaluation_result(current_element,text);
+
+}
+
+
+
+
+/**
+ * @typedef {Object} Tag23LoopProps
+ * @property {Element || HTMLInputElement} current_element
+ * @property {number} index
+ * @property {boolean} skip
+ * */
+
+
+
+
+/**
+ * @param {Tag23LoopProps} loop_props
+ */
+function tag23_execute_main_loop_actions(loop_props){
+
+    let current_element = loop_props.current_element;
+
+    let callbacks = {
+        [TAG_23_START]:()=> tag23_start(current_element),
+        [TAG_23_CASE]:()=> tag23_case(loop_props),
+        [TAG_23_UNLESS]:()=> tag23_unless(loop_props),
+        [TAG_23_DEFAULT_VALUE]:()=> tag23_value(current_element),
+        [TAG_23_SET_VALUE]:()=> tag23_set_value(current_element),
+        [TAG_23_PRINT]:()=> tag23_print(current_element),
+        [TAG_23_FOR]:()=> tag23_for(loop_props)
+    }
+
+    for(let attribute in callbacks){
+        if(current_element.hasAttribute(attribute)){
+
+            try{
+                callbacks[attribute]();
+            }
+
+            catch (error){
+                console.error(error);
+            }
+        }
+    }
+
+
+}
+
+/**
+ * @param {HTMLElement || Document} target
+ * */
+function run_loop(target){
+
+
+    for(let i=0;i<target.children.length;i++){
+
+        /**@type {Tag23LoopProps}*/
+        let loop_props = {
+            current_element: target.children[i],
+            index:i,
+            skip:false
+        }
+        tag23_execute_main_loop_actions(loop_props);
+        i = loop_props.index;
+        
+        if(!loop_props.skip){
+            run_loop(loop_props.current_element);
+        }
+
+
+    }
+
+}
+function start(){
+    
+
+
+    run_loop(document);
+    let time = TAG_23_TICK_DEFAULT_TIME;
+
+    try{
+        //maybe the user can define it
+        if(TAG_23_TICk_TIME){
+            time = Number(TAG_23_TICk_TIME);
+        }
+    }catch (error){}
+        setInterval(function(){
+        run_loop(document.body);
+    },time);
+}
+
+window.addEventListener('load',start);
+
+
