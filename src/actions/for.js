15c953037f3548e625336e5a41bdec3aaeac903b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-22 00:11:06 -0300

    att

--- src/actions/for.js
+++ src/actions/for.js
@@ -1,35 +1,53 @@
 
-function tag23_get_old_elements(father,index,iterator){
+/**@param {Tag23LoopProps} loop_props
+ * @return {Array<HTMLElement>}
+ */
+function tag23_get_old_elements(loop_props){
+    /**@type {Array<HTMLElement || ChildNode>}*/
     let elements = [];
-    for(let i = 0;i< father.children.length;i++){
-        let point = i+index+1;
-        let current = father.children[point];
-        if(!current){
+    let current_element = loop_props.current_element.previousSibling;
+    while(current_element){
+        current_element = current_element.previousSibling;
+        if(!current_element){
             break;
         }
 
-        if(!current.hasAttribute(TAG_23_INDEX)){
-            let name = current.getAttribute(TAG_23_IN);
-            if(name !== iterator){
-                break;
-            }
+        if(current_element.element_of === loop_props.current_element){
+            elements.push(current_element);
         }
-
-
-        elements.push({
-            index: parseInt(current.getAttribute(TAG_23_INDEX)),
-            order:i,
-            element:current
-        });
     }
-    return elements;
+    return  elements;
 }
 
-
+/**
+ * @param {Array<HTMLElement>} old_elements
+ * @param {number} array_size
+ */
+function  tag_23_remove_higher_elements(old_elements,array_size){
+    for(let i = array_size; i < old_elements.length; i++){
+        old_elements[i].remove();
+    }
+}
+/**
+ * @param {HTMLElement} current_element
+ * @param {Array<HTMLElement>} old_elements
+ * @param {number} array_size
+ */
+function tag_23_insert_clones(current_element,old_elements,array_size){
+    for(let i = old_elements.length; i < array_size; i++){
+        let clone = current_element.cloneNode(true);
+        clone.style.display = TAG_23_SHOW;
+        clone.removeAttribute('for');
+        clone.element_of = current_element;
+        let father  = current_element.parentElement;
+        father.insertBefore(clone,current_element);
+    }
+}
 
 /**@param {Tag23LoopProps} loop_props
  */
 function tag23_for(loop_props){
+
     loop_props.skip = true;
     let current = loop_props.current_element;
     current.style.display = TAG_23_HIDE;
@@ -39,8 +57,6 @@ function tag23_for(loop_props){
         console.log("for value not provided")
         return;
     }
-    let index_name = `${value_as}_${TAG_23_INDEX}`
-    let destroy_name = `${value_as}_${TAG_23_DESTROY}`
     let array_name =   current.getAttribute(TAG_23_IN);
     if(!array_name){
         console.log("in not provided")
@@ -48,91 +64,14 @@ function tag23_for(loop_props){
     }
     /**@type {Array<any>}*/
     let array_value = tag23get_evaluation_result(current,array_name);
-    if(!array_value){
-        return;
+
+    let old_elements = tag23_get_old_elements(loop_props);
+    if(old_elements.length > array_value.length){
+        tag_23_remove_higher_elements(old_elements,array_value.length);
     }
-    //iterated over the brothers of child
-    let father = current.parentNode;
-    let elements = tag23_get_old_elements(father,loop_props.index,array_name);
-
-
-
-    for(let j=0;j<array_value.length;j++){
-        //verify if j its not present on elements
-        let found = false;
-        for(let k=0;k<elements.length;k++){
-            if(elements[k].index === j){
-                found = true;
-                break;
-            }
-        }
-        if(found){
-            continue;
-        }
-
-        let created = current.cloneNode(true);
-        created.style.display = TAG_23_SHOW;
-        //remove attribute for
-        created.removeAttribute(TAG_23_FOR);
-
-        set_value_recursively_in_element(created,value_as,()=>{
-            return array_value[j];
-        })
-
-        set_value_recursively_in_element(created,index_name,()=>{
-            return j;
-        })
-
-        set_value_recursively_in_element(created,destroy_name,()=>{
-            array_value.splice(j,1);
-        })
-
-
-        created.setAttribute(TAG_23_INDEX,j);
-        elements.push({
-            index:j,
-            element:created
-        });
+    if(old_elements.length < array_value.length){
+        tag_23_insert_clones(current,old_elements,array_value.length);
     }
-
-
-
-    //sort elements by index
-    elements.sort((a,b)=>a.index-b.index);
-    let its_all_correct = true;
-    for(let j=0;j<elements.length;j++){
-        if(elements[j].order !== j){
-            its_all_correct = false;
-            break;
-        }
-    }
-
-    if(its_all_correct && elements.length === array_value.length){
-        return;
-    }
-
-    //console.log(elements);
-    //console.log(array_value);
-
-
-    let fragment = document.createDocumentFragment();
-    for(let j=0;j<elements.length;j++){
-        //verify if j its not present on elements
-        let current = elements[j];
-
-
-
-        if(current.element.parentNode === father){
-            current.element = father.removeChild(current.element);
-        }
-        if(current.index <= array_value.length -1){
-            fragment.appendChild(current.element);
-        }
-
-    }
-    father.insertBefore(fragment,current.nextSibling);
-
-
 
 }
 
--- src/globals.js
+++ src/globals.js
@@ -2,6 +2,7 @@ let TAG_23_CURRENT_TICK = 0;
 let TAG_23_TIME_PASSED = 0;
 /**@type {Array<function>}*/
 let TAG_23_MAIN_LOOP_CALLBACKS = [];
+
 let TAG_23_TICK_TIME = 40;
 /**@type {Array<string>}*/
 let TAG_23_SHOWED_MESSAGES = [];
